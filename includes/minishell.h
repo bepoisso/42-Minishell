06e12cfeca8154d528cccf3ff467d790e16e22dd
Zeph <jrinaudo@student.42perpignan.fr>
2025-02-12 20:27:48 +0100

    add minishel.h utilz dir and handle_sig.c

---
+++ includes/minishell.h
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrinaudo <jrinaudo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/12 08:08:11 by jrinaudo          #+#    #+#             */
+/*   Updated: 2025/02/12 10:37:27 by jrinaudo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+/*used for printf, perror */
+# include <stdio.h>
+/*used for malloc, free, exit, getenv */
+# include <stdlib.h>
+/*used for access, fork, read, write, close, getcwd, chdir, execve, dup, dup2,
+ pipe, isatty, ttyname, ttyslot */
+# include <unistd.h>
+/*used for strerror */
+# include <string.h>
+/*used for errno (used with perror, strerror) */
+# include <errno.h>
+/*used for wait, waitpid, wait3, wait4*/
+# include <sys/wait.h>
+/*used for system types (fork, wait, etc.)*/
+# include <sys/types.h>
+/*used for stat, lstat, fstat */
+# include <sys/stat.h>
+/*usedd to retrieve resource usage information(wait3, wait4)*/
+# include <sys/resource.h>
+/*used for open et les options de contrôle de fichier */
+# include <fcntl.h>
+/*used for signal, sigaction, sigemptyset, sigaddset, kill */
+# include <signal.h>
+/*used for readline, rl_on_new_line, rl_replace_line, rl_redisplay */
+# include <readline/readline.h>
+/*used for rl_clear_history, add_history */
+# include <readline/history.h>
+/*used for PATH_MAX (with getcwd) */
+# include <limits.h>
+/*used for ioctl */
+# include <sys/ioctl.h>
+/*used for tcsetattr, tcgetattr */
+# include <termios.h>
+/*used for tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs */
+# include <term.h>
+
+# define BLUE "\033[0;34m"
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
+# define CYAN "\033[0;36m"
+# define RESET "\033[0m"
+
+#endif
---
+++ srcs/sighand.c
@@ -0,0 +1,89 @@
+#include "../include/minishell.h"
+
+/**
+ * @brief Signal handler function for SIGINT (CTRL-C)
+ * 
+ * This function is called when a SIGINT signal (CTRL-C) is received.
+ * It prints a blue-colored message indicating that CTRL-C was received,
+ * followed by resetting the color to default.
+ * 
+ * @return void
+ */
+void	sig_int_handler()
+{
+	printf(""BLUE"Signal CTRL-C received"RESET"\n");
+	exit (0);
+}
+
+
+/**
+ * sigemptyset(&sa_int.sa_mask)
+ * The sigemptyset() function initializes the signal set set to empty, with
+ *  all signals excluded from the set.
+ * you guarantee that this mask does not contain any signal. The sa_mask field
+ *  is used to indicate which signals should be temporarily blocked while the
+ *  signal handler is running. By flushing it, you ensure that no signals are
+ *  blocked by default while your handler for SIGINT is running. You can then,
+ *  if necessary, add specific signals to block using sigaddset.
+ */
+int	sig_handler(void)
+{
+	struct sigaction	sa_int;
+	struct sigaction	sa_quit;
+
+	sa_int.sa_flags = SA_RESTART | SA_SIGINFO;
+	sigemptyset(&sa_int.sa_mask);
+	sa_int.sa_handler = sig_int_handler;
+	if (sigaction(SIGINT, &sa_int, NULL) == -1)
+    {
+        perror("sigaction SIGINT");
+        exit(EXIT_FAILURE);
+    }
+	
+	sa_quit.sa_flags = SA_RESTART | SA_SIGINFO;
+	sigemptyset(&sa_quit.sa_mask);
+	sa_quit.sa_handler = SIG_IGN;
+	if (sigaction(SIGQUIT, &sa_quit, NULL) == -1)
+	{
+		perror("sigaction SIGQUIT");
+		exit(EXIT_FAILURE);
+	}
+	while (1)
+		sleep(1);
+	return (0);
+}
+/*struct sigaction {
+	void     (*sa_handler)(int);
+	void     (*sa_sigaction)(int, siginfo_t *, void *);
+	sigset_t sa_mask;
+	int      sa_flags; SA_SIGINFO doit etre defini pour utiliser sa_sigaction
+	plutot que sa_handler
+};
+siginfo_t {
+	int      si_signo;    Signal number
+	int      si_errno;    An errno value
+	int      si_code;     Signal code
+	int      si_trapno;   Trap number that caused
+							 hardware-generated signal
+							 (unused on most architectures)
+	pid_t    si_pid;      Sending process ID
+	uid_t    si_uid;      Real user ID of sending process
+	int      si_status;   Exit value or signal
+	clock_t  si_utime;    User time consumed
+	clock_t  si_stime;    System time consumed
+	sigval_t si_value;    Signal value
+	int      si_int;      POSIX.1b signal
+	void    *si_ptr;      POSIX.1b signal
+	int      si_overrun;  Timer overrun count; POSIX.1b timers
+	int      si_timerid;  Timer ID; POSIX.1b timers
+	void    *si_addr;     Memory location which caused fault
+	int      si_band;     Band event
+	int      si_fd;       File descriptor
+};
+*/
+
+int	main(void)
+{
+	sig_handler();
+	return (0);
+}

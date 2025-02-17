71410e48e359b96a589aecdfc0dcd72713acf86d
bepoisso <poisson.benjamin69@gmail.com>
2025-02-17 16:36:47 +0100

    WIP : token handling

--- srcs/p_token.c
+++ srcs/p_token.c
@@ -1,5 +1,19 @@
 #include "../includes/parser.h"
 
+/* 
+INDEX	TYPE	DESCRIPTION
+1		infile	infile
+2		outfile	outfile
+3		<		redir infile replace
+4		>		redir outfile replace
+5		<<		redir infile append
+6		<<		redir outfile append
+7		|		pipe
+8		$		variable
+9		cmd		commande
+10		args	commande args
+*/
+
 t_token	*create_token(char *value)
 {
 	t_token	*new;
@@ -33,6 +47,7 @@ void	add_token(t_token **tokens, char *value)
 		temp->next = new;
 		new->prev = temp;
 	}
+	free(value);
 }
 
 t_token	*tokenizer(char *s)

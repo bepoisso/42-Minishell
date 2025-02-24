6a00b54bab386f54741be07689eb23c6b5818541
bepoisso <poisson.benjamin69@gmail.com>
2025-02-24 11:59:29 +0100

    Update : struct add thigs

--- includes/parser.h
+++ includes/parser.h
@@ -8,21 +8,24 @@
 */
 typedef struct s_cmd
 {
-	char	**cmd;
-	char	*path_cmd;
-	bool	builtin;
-	struct s_cmd *next;
-	struct s_cmd *prev;
+	char			**cmd;
+	char			*path_cmd;
+	bool			builtin;
+	struct s_cmd	*next;
+	struct s_cmd	*prev;
 }	t_cmd;
 
-/* 
-	Struct for the token
+/**
+ * Struct for the token
+ * j'ai ajoute index_pipe ca va m'aider a gerer naviguer dans le tableau de
+ * pipes, comme ca qund je navigue dans la liste et que j'arrive sur un pipe,
+ *  je sais comment le gerer avec le tableau de pipes
 */
 typedef struct s_token
 {
 	int				id;
 	char			*data;
-	bool			literal;
+	int				index_pipe;
 	struct s_token	*next;
 	struct s_token	*prev;
 }	t_token;
@@ -32,9 +35,13 @@ typedef struct s_token
 */
 typedef struct s_base
 {
-	int		exit_code;
-	t_cmd	*cmds;
-	t_token	*token;
+	int				exit_code;
+	int				count_forks;
+	int				count_pipe;
+	int				**pipes;
+	char			**path_list;
+	t_cmd			*cmds;
+	t_token			*token;
 }	t_base;
 
 

fd83d29b7e1637b89ff067f085e7a46e0c66abac
bepoisso <poisson.benjamin69@gmail.com>
2025-03-17 15:30:12 +0100

    FIX

--- includes/builtins.h
+++ includes/builtins.h
@@ -9,7 +9,6 @@ char	**env_cpy(void);
 void	builtin_env(t_base *base, t_token *actual_tok);
 void	builtin_pwd(t_base *base);
 void	builtin_echo(t_base *base, t_token *actual_tok);
-void	builtin_exit(t_base *base);
 void	builtin_cd(t_base *base, t_token *actual_tok);
 void	builtin_export(t_base *base, t_token *actual_tok);
 

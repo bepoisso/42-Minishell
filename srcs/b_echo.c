2df48ef2688d69596b2053073bb037dfaec49b89
bepoisso <poisson.benjamin69@gmail.com>
2025-03-20 12:08:06 +0100

    UPDATE Handling echo args

--- srcs/b_echo.c
+++ srcs/b_echo.c
@@ -1,19 +1,46 @@
 #include "../includes/minishell.h"
 
+int	bypass_args(char *arg)
+{
+	int		j;
+
+	j = 0;
+	if (arg[j] && ft_strncmp(arg , "-n", 2) == 0)
+	{
+		j = 1;
+		while (arg[j] == 'n')
+		j++;
+		if (arg[j] == '\0')
+			return (true);
+	}
+	return (false);
+}
+
 void	builtin_echo(t_base *base, t_token *actual_tok)
 {
 	int		i;
 	char	**cmd;
+	bool	line_return;
+	bool	printing;
 
 	i = 1;
+	printing = false;
 	cmd = actual_tok->cmd->cmd;
 	while (cmd[i])
 	{
+		if (printing == false && bypass_args(cmd[i]) == true)
+		{
+			line_return = false;
+			i++;
+			continue;
+		}
 		ft_putstr(cmd[i]);
+		printing = true;
 		if (cmd[i + 1])
 			ft_putchar(' ');
-		i++;
+		i++; 
 	}
-	ft_putchar('\n');
+	if (line_return)
+		ft_putchar('\n');
 	base->exit_code = 0;
 }
